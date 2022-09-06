fef5905b68e968af5695dc0a9a68dad0efdf5fd6
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2022-09-06 14:49:13 +0800

    initial conmmit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: const HomePage(title: 'Flutter Demo Home Page'),
+      );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.headline4,
+        ),
+      ),
+    );
+  }
+}
