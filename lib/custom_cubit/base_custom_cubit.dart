7e48a12b61aa0618f9fcc8809dae109b04791277
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2022-09-08 15:31:56 +0800

    add base service

--- lib/custom_cubit/base_custom_cubit.dart
+++ lib/custom_cubit/base_custom_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_poc/custom_cubit/base_custom_service.dart';
 
 import 'package:flutter_bloc_poc/custom_cubit/base_dependency_resolver.dart';
 import 'package:flutter_bloc_poc/custom_cubit/base_entity.dart';
@@ -8,12 +9,14 @@ import 'package:flutter_bloc_poc/custom_cubit/custom_state.dart';
 abstract class BaseCustomCubit<E extends BaseEntity>
     extends Cubit<CustomState> {
   BaseCustomCubit({
+    required this.service,
     required this.dependencyResolver,
     required CustomState<Equatable> initialState,
   }) : super(initialState) {
     dependencyResolver.resolve().distinct().listen(handler);
   }
 
+  final BaseCustomService service;
   final BaseDependencyResolver<E> dependencyResolver;
 
   void handler(E data);
